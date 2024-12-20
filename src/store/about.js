31d347698249811aa5a0333693b2feeb5bea69cb
DongHeeLee-dev <41ehdgml@naver.com>
2024-12-20 13:47:56 +0900

    Create project

---
+++ src/store/about.js
@@ -0,0 +1,9 @@
+import { Store } from '../core/heropy'
+
+export default new Store({
+  photo: 'https://heropy.blog/css/images/logo.png',
+  name:  'DongHeeLee',
+  email: '41ehdgml@gmail.com',
+  github: 'https://github.com/DongHeeLee-dev',
+  repository: 'https://github.com/DongHeeLee-dev'
+})
